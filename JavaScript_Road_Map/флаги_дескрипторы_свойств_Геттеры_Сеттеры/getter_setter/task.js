8c3c6f2be9c53c40c7d2912eeef0c8089d313623
Maksim hmelnickiy <maksimhmelnickiy@Maksims-MacBook-Air.local>
2023-08-22 21:56:33 +0300

    getter and setter flafs deskriptors

---
+++ JavaScript_Road_Map/флаги_дескрипторы_свойств_Геттеры_Сеттеры/getter_setter/task.js
@@ -0,0 +1,49 @@
+// Задача: Учет банковского счета без классов
+
+// Ваша задача - создать объект для учета банковского счета без использования классов. 
+// Используйте функции (геттеры и сеттеры) для управления данными счета и проверки минимального баланса.
+
+// Создайте объект bankAccount с двумя свойствами: balance (начальный баланс) и minBalance (минимальный допустимый баланс).
+
+// Создайте геттер currentBalance, который будет возвращать текущий баланс счета.
+
+// Создайте сеттер deposit(amount), который позволит вносить деньги на счет. Проверьте, что сумма депозита положительная, и обновите баланс.
+
+// Создайте сеттер withdraw(amount), который будет позволять снимать деньги со счета. Проверьте, что сумма снятия положительная
+//  и не превышает текущий баланс. Если сумма снятия больше текущего баланса, выведите сообщение о недостатке средств.
+
+// Создайте геттер isBalanceLow, который будет возвращать true, если текущий баланс меньше минимально допустимого баланса,
+//  и false в противном случае.
+
+const bankAccount = {
+  balance: 300,
+  minBalance: 50,
+  get currentBalance(){
+    return this.balance
+  },
+  set deposit(amount){
+    if(amount > 0){
+      return this.balance += amount
+    }
+    return console.error('Депозит должен быть больше нуля')
+  },
+  set withdraw(amount){
+    if(this.balance > amount && amount > 0){
+      return this.balance -= amount
+    }
+    return console.error('Недостаточно средств')
+  },
+  get isBalanceLow(){
+    if(this.balance < this.minBalance){
+      return true
+    }
+    return false
+  }
+}
+
+console.log(bankAccount.currentBalance) // 300
+bankAccount.deposit = 100
+console.log(bankAccount.currentBalance)
+bankAccount.withdraw = 100
+console.log(bankAccount.currentBalance)
+console.log(bankAccount.isBalanceLow);
