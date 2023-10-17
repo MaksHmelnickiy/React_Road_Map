41e77007f014357d24cadc43b5c2c09d3616fffd
Maksim hmelnickiy <maksimhmelnickiy@Maksims-MacBook-Air.local>
2023-10-17 05:44:04 +0300

    t

--- cra-typescript-example/src/LearnReact/React_Router/Пример_Авторизации/pages/Login.tsx
+++ cra-typescript-example/src/LearnReact/React_Router/Пример_Авторизации/pages/Login.tsx
@@ -1,11 +1,30 @@
+import React from "react";
 import { useLocation, useNavigate } from "react-router-dom"
+import { useAuth } from "../hook/useAuth";
+
 
 export const Login = () => {
   const navigate = useNavigate();
   const location = useLocation();
-  const fromPage = location.state.from.pathname || '/'
+  const fromPage = location.state.from.pathname || '/';
+  const auth = useAuth()
+  const handleSubmit = React.useCallback((e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const form = e.target as HTMLFormElement;
+    const value = (form.querySelector('input[name="username"]') as HTMLInputElement).value
+    signIn(value, () => navigate('authTrue'))
+  },[navigate])
+  if(!auth){
+    return null
+  }
+  const { signIn } = auth;
+
   return <div>Page Login;  
            <div>You come from page: {fromPage}
            </div>
+           <form onSubmit={handleSubmit}>
+            <input type="text" name="username" />
+            <button type="submit">Login</button>
+           </form>
          </div>
 }
