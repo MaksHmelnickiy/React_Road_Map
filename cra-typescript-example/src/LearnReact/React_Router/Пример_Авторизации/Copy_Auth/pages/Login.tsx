733b94da6a45cb1f769fa13976aacd769d36bfbb
Maksim Hmelnickiy <makshmelnickiy@gmail.com>
2023-10-17 09:23:01 +0300

    correct

--- cra-typescript-example/src/LearnReact/React_Router/Пример_Авторизации/Copy_Auth/pages/Login.tsx
+++ cra-typescript-example/src/LearnReact/React_Router/Пример_Авторизации/Copy_Auth/pages/Login.tsx
@@ -3,24 +3,30 @@ import { useAuth } from "../hook.tsx/useAuth"
 import { useLocation, useNavigate } from "react-router-dom"
 
 export const Login = () => {
-  const auth = useAuth()
-  const navigate = useNavigate()
-  const location = useLocation()
-  const from = location.state.from.pathname
-  const onSubmit = React.useCallback((e:React.FormEvent) => {
-    e.preventDefault()
+  const auth = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state.from.pathname;
+
+  const onSubmit = React.useCallback((e: React.FormEvent, fromPath: string) => {
+    e.preventDefault();
     const form = e.target as HTMLFormElement;
-    const value = form.userName.value 
-    auth?.signIn(value, () => navigate(from, {replace: true}))
-  },[])
-  return <div>
-    <h1>Login</h1>
-    <form onSubmit={onSubmit}>
-      <label>
-        Name: 
-        <input type="text" name='userName' />
-      </label>
-      <div><button type="submit">Send</button></div>
-    </form>
-  </div>
-}
+    const value = form.userName.value;
+    auth?.signIn(value, () => navigate(fromPath, { replace: true }));
+  }, []);
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={(e) => onSubmit(e, from)}>
+        <label>
+          Name:
+          <input type="text" name='userName' />
+        </label>
+        <div>
+          <button type="submit">Send</button>
+        </div>
+      </form>
+    </div>
+  );
+};
