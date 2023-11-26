7b339a90eb8359a0dd5b76589b3830b225ab931f
Maksim hmelnickiy <maksimhmelnickiy@Maksims-MacBook-Air.local>
2023-11-26 05:06:11 +0200

    te

--- JavaScript_Road_Map/dataStructures/Array/arrMethod/Array.from.js
+++ JavaScript_Road_Map/dataStructures/Array/arrMethod/Array.from.js
@@ -14,9 +14,24 @@ let arrayLike = {
   }
   console.log(arr); // World (метод работает)
 
+  const obj3 = {
+    first: 'maks',
+    second: 'hmel',
+    age: 33
+  }
+
+  const arr3 = Array.from(obj3)
+  console.log(arr3)
+
 // Что бы превратить массив в объект используйте оператор ⁡⁢⁣⁣Spread - {...arr}⁡ или  ⁡⁢⁣⁣Object.assign({}, array)⁡;
+
+// ⁡⁢⁣⁣Object.assign()⁡ — это метод в JavaScript, используемый для ⁡⁣⁣⁢копирования значений⁡ всех собственных перечисляемых свойств 
+// из одного или нескольких исходных объектов в целевой объект. После копирования он возвращает этот целевой объект. 
+// Этот метод часто используется для создания копий объектов ⁡⁣⁣⁢и⁡⁡⁣⁣⁢ли для слияния нескольких объектов в один⁡.
+
 const array = ['foo', 'boo', 'zoo'];
 const obj = Object.assign({}, array);   // ⁡⁢⁣⁡⁢⁣⁣Object.assign({}, array)⁡ /
+console.log(obj)
 
 const arrSp = ["foo", "boo", "zoo"];
 const obj2 = {...arr};                  // ⁡⁢⁣⁡⁢⁣⁣Spread - {...arr}⁡ /
