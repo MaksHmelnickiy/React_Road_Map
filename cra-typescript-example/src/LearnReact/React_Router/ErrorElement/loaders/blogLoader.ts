cba7d387e153d58432ba265db8c61bf5f99d03ab
Maksim Hmelnickiy <makshmelnickiy@gmail.com>
2023-12-28 15:29:41 +0200

    err

--- cra-typescript-example/src/LearnReact/React_Router/ErrorElement/loaders/blogLoader.ts
+++ cra-typescript-example/src/LearnReact/React_Router/ErrorElement/loaders/blogLoader.ts
@@ -1,13 +1,22 @@
-import { defer } from "react-router-dom"
+import { defer, json } from "react-router-dom"
 
 async function getPosts() {
   const resp = await fetch('https://jsonplaceholder.typicode.com/posts')
+  // - это ⁡⁣⁣⁢первый вариант для работы с ошибками⁡⁡. Мы выбрасываем ⁡⁢⁣⁣Reasponse⁡ и на странице ⁡⁣⁣⁢ErrorPage⁡ его получаем.
   if(!resp.ok){
-    throw new Response('', {status: resp.status, statusText: 'Error comes!'})
+    throw new Response('', {status: resp.status, statusText: 'Error comes!'}) 
+
+    // ⁡⁣⁢⁣new⁡ ⁡⁢⁣⁣Response⁡ - это ⁡⁣⁣⁢конструктор⁡ ⁡⁣⁣⁢объекта⁡ ⁡⁢⁣⁣Response⁡ в JavaScript, который часто используется в контексте работы с ⁡⁣⁣⁢сетевыми запросами⁡, например, при использовании ⁡⁢⁣⁣API Fetch⁡.
+    // представляет собой специальный способ ⁡⁣⁣⁢работы с ответами от сервера⁡, вы можете получать и обрабатывать информацию ⁡⁣⁣⁢после отправки запросов⁡ в интернете ⁡⁣⁣⁢HTTP-запроса⁡. 
   }
   return await resp.json()
 }
 
 export const blogLoader = async () => {
+  // const posts = getPosts() as any
+  // if(!posts.length){
+  //   throw json({message: 'error page now'}, {status: 404}) // это ⁡⁣⁣⁢второй способ⁡ с использованием ⁡⁢⁣⁣json()⁡ из самой либы ⁡⁣⁢⁣React Router⁡ - он принимает два объекта. 
+  //   // И получать их потом на странице ⁡⁢⁣⁣ErrorPage⁡  например {error.data.message} // За ⁡⁢⁣⁣json⁡ ⁡⁢⁢⁢лучше почитать⁡ еще где то. 
+  // }
   return defer({posts: getPosts()})
 }
--- cra-typescript-example/src/LearnReact/React_Router/ErrorElement/pages/ErrorPage.tsx
+++ cra-typescript-example/src/LearnReact/React_Router/ErrorElement/pages/ErrorPage.tsx
@@ -3,7 +3,8 @@ import { ErrorResponse, isRouteErrorResponse, useRouteError } from "react-router
 export const ErrorPage = () => {
   const error = useRouteError() as ErrorResponse;
   console.log(isRouteErrorResponse(error)) // Возвращает ⁡⁢⁣⁣true⁡ или ⁡⁢⁣⁣false⁡ если ⁡⁢⁣⁢ошибка⁡ была в самом ⁡⁣⁣⁢роуте маршрута⁡. 
-                                           // Например: ⁡⁣⁣⁢клик по линке⁡ с ⁡⁢⁣⁢несуществующем⁡ ⁡⁣⁣⁢маршрутом⁡ /
+                                           // Например: ⁡⁣⁣⁢клик по линке⁡ с ⁡⁢⁣⁢несуществующем⁡ ⁡⁣⁣⁢маршрутом⁡ 
+                                           // В данной папке у нас есть ⁡⁣⁣⁢ссылка⁡ на страницу ⁡⁢⁣⁣contact⁡, которой ⁡⁢⁣⁢не существует⁡ и тамм оно выведет ⁡⁣⁢⁣true⁡ /
                                            
   return <h2>{error.status} <br /> {error.statusText}</h2>
 }
